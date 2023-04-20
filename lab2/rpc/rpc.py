4446c3fff5b583a867bf7db78723321500a55221
vladi_01 <vladimir.alyoshin@bieber-systems.com>
2023-04-20 12:54:18 +0200

    lab-2: implemented lab2 threading im client

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,6 +1,9 @@
 import constRPC
+import threading
+import time
 
 from context import lab_channel
+from image import image
 
 
 class DBList:
@@ -12,25 +15,57 @@ class DBList:
         return self
 
 
-class Client:
+class Client(threading.Thread):
     def __init__(self):
+        threading.Thread.__init__(self)
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
         self.server = None
-
-    def run(self):
         self.chan.bind(self.client)
         self.server = self.chan.subgroup('server')
+        self.queue = []
+
+    def run(self):
+        print("[Thread] Waiting for response")
+        received_message = self.chan.receive_from(self.server)
+        print("[Thread] Response received")
+        self.queue = received_message[1]
+
 
     def stop(self):
         self.chan.leave('client')
+
+    def ack_waiting(self):
+        received_message = self.chan.receive_from(self.server)
+        if received_message[1] == constRPC.OK :
+            print("[Client] ACK received")
+            return
 
     def append(self, data, db_list):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
         self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        self.ack_waiting()
+        self.start()
+        time.sleep(1)
+        print("[Client] Here client does something")
+        time.sleep(1)
+        print("[Client] Here is a simulation of work")
+        time.sleep(1)
+        print("[Client] Are you boring?")
+        time.sleep(1)
+        print("[Client] I can show something to you")
+        time.sleep(1)
+        print("[Client] Ready?")
+        time.sleep(1)
+        print("[Client] Don't be so shy")
+        time.sleep(1)
+        print(image)
+        time.sleep(1)
+        print("[Client] Hopla!")
+        self.join()
+        print("[Thread] closed...")
+        return self.queue
 
 
 class Server:
@@ -44,6 +79,10 @@ class Server:
         assert isinstance(db_list, DBList)  # - Make sure we have a list
         return db_list.append(data)
 
+    @staticmethod
+    def send_ack(client, chan):
+        chan.send_to({client}, constRPC.OK)
+
     def run(self):
         self.chan.bind(self.server)
         while True:
@@ -52,7 +91,19 @@ class Server:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
+                    self.send_ack(client, self.chan)
+                    request_processing_timer(10)
+                    print("\n[Server] Request processed")
                     result = self.append(msgrpc[1], msgrpc[2])  # do local call
                     self.chan.send_to({client}, result)  # return response
                 else:
                     pass  # unsupported request, simply ignore
+
+def request_processing_timer(seconds):
+    print("[Server] Request processing")
+    i = 1
+    while i < seconds:
+        dots = '.' * i
+        print("\r{}".format(dots), flush=True, end='')
+        time.sleep(1)
+        i += 1
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -6,7 +6,6 @@ from context import lab_logging
 lab_logging.setup(stream_level=logging.INFO)
 
 cl = rpc.Client()
-cl.run()
 
 base_list = rpc.DBList({'foo'})
 result_list = cl.append('bar', base_list)
